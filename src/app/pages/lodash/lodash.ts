1a14e089c2593f146c4835e75d72bddeef8d8a4e
FengOS <18636553220@163.com>
2017-01-24 11:14:40 +0800

    add difference drop doc

--- src/app/pages/lodash/lodash.ts
+++ src/app/pages/lodash/lodash.ts
@@ -8,19 +8,47 @@ import * as _ from 'lodash';
 })
 
 export class Lodash {
-  
+
 
   chunk() {
     let _array = [1, 2, 3, 4, 5, 6, 7, 8];
-    console.log('array',_array);
+    console.log('array', _array);
     console.log('chunk', _.chain(_array).chunk(2).value());
-    console.log('chunk', _.chain(_array).chunk(3).value());   
+    console.log('chunk', _.chain(_array).chunk(3).value());
   }
 
   compact() {
-    let _array = [ 1, false, 2 , null, '', 0, [], undefined, 3, NaN, 4 ];
-    console.log('array',_array);
-    console.log('compact',_.chain(_array).compact().value());
+    let _array = [1, false, 2, null, '', 0, [], undefined, 3, NaN, 4];
+    console.log('array', _array);
+    console.log('compact', _.chain(_array).compact().value());
+  }
+
+  concat() {
+    let _array = [1];
+    console.log('array', _array);
+    console.log('concat', _(_array).concat([3], [[4]]).value());
+  }
+
+  differenceBy() {
+    console.log('differenceBy', _([1, 2]).differenceBy([2, 3]).value());
+  }
+
+  differenceWith() {
+    console.log('differenceWith', _.differenceWith([1, 2, 5], [2, 3], function (a, b) {
+      console.log(a, b);
+      return a == b;
+    }));
+  }
+
+  dropRightWhile() {
+    var users = [
+      { 'user': 'barney', 'active': true },
+      { 'user': 'fred', 'active': false },
+      { 'user': 'pebbles', 'active': false }
+    ];
+    console.log(_.dropRightWhile(users,function(o) {
+      return !o.active;
+    }))
   }
 
 }
